dd5c078fb4eb00b14648c86e598d2630f4179532
edwin <edwinrenepobrebolanos@gmail.com>
2024-02-14 22:11:23 -0300

    photo album using c#

---
+++ README.md
@@ -0,0 +1,8 @@
+# CSharpTest
+
+This project was generated with Visual Studio
+
+## Development server
+
+you can open solution (.sln) file using visual studio and run
+
---
+++ photoalbum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+HttpClient client = new HttpClient();
+
+try
+{
+    string albumId = "2"; // Change this to the albumId you want to fetch.
+
+    HttpResponseMessage response = await client.GetAsync($"https://jsonplaceholder.typicode.com/photos?albumId={albumId}");
+
+    response.EnsureSuccessStatusCode();
+
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseBody);
+}
+
+catch (HttpRequestException e)
+{
+    Console.WriteLine($"\nException Caught!");
+    Console.WriteLine($"Message: {e.Message}");
+}
